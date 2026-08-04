04544625e5a017ad3dca42d4147874007e4b80be
Shinzie471 <johnjofreybalbal@gmail.com>
2026-08-04 14:32:59 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -45,3 +45,11 @@ self.addEventListener('fetch', (event) => {
     })
   );
 });
+
+// Support skipWaiting via postMessage from the page
+self.addEventListener('message', (event) => {
+  if (!event.data) return;
+  if (event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
